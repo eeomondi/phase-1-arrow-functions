de01edec3648735d8daae988bd37c732feee3059
eeomondi <elvis.omondi@student.moringaschool.com>
2024-07-10 18:42:03 +0300

    commit

---
+++ index.js
@@ -0,0 +1,13 @@
+function divide(a, b) {
+    return a / b;
+  }
+  
+  function square(a) {
+    return a * a;
+  }
+  
+  function add(a, b) {
+    return a + b;
+  }
+  
+  module.exports = { divide, square, add };
