fb26080ee5edaa9e7e0086f89e7baf2492b2bdc6
eeomondi <elvis.omondi@student.moringaschool.com>
2024-07-17 16:10:04 +0300

    commit

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,35 +1,6 @@
 require ( './helpers.js' );
 
-const { divide, square, add } = require('./index');
-
-describe('index.js', () => {
-  it('has a function expression called divide', () => {
-    expect(divide).toBeDefined();
-  });
-
-  it('divide divides 2000 by 100', () => {
-    expect(divide(2000, 100)).toBe(20);
-  });
-
-  it('has an arrow function called square', () => {
-    expect(square).toBeDefined();
-  });
-
-  it('square arrow function takes one parameter and multiplies it times itself', () => {
-    expect(square(4)).toBe(16);
-  });
-
-  it('has an arrow function called add', () => {
-    expect(add).toBeDefined();
-  });
-
-  it('add arrow function takes two parameters and adds them together', () => {
-    expect(add(2, 3)).toBe(5);
-  });
-});
-
-
-/*describe("index.js", () => {
+describe("index.js", () => {
   it("has a function expression called divide", () => {
     expect(divide).to.exist
   })
@@ -58,5 +29,4 @@ describe('index.js', () => {
     let b = 4
     expect(add(a,b)).to.equal(a+b)
   })
-})*/
-
+})
