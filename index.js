72ac4d623b170a77af2ecf3dce3baeeba0228e1f
Elvis <elvis.omondi@student.moringaschool.com>
2024-07-21 13:02:42 +0300

    commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+const divide = function(a, b) {
+    return a / b;
+  };
+  
+  const square = (x) => {
+    return x * x;
+  };
+  
+  const add = (a, b) => {
+    return a + b;
+  };
